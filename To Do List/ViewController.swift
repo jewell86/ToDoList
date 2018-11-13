5a3b6823dff5610082d62897b43628045a44bad0
jewell86 <jewellsmailbox@gmail.com>
2018-11-13 11:26:28 -0800

    initialize Swift project

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Jewell Braden on 11/13/18.
+//  Copyright © 2018 Jewell White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
